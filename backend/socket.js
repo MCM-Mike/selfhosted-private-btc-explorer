0f60de06188114a3f9d96a805f350070dfc797df
Mick <mickvd99@gmail.com>
2021-04-24 18:38:32 +0200

    added socket functions to get blocks and transactions

--- backend/socket.js
+++ backend/socket.js
@@ -7,6 +7,9 @@ function socketIo(io) {
 
     socket.on('getBlockCount', () => { getBlockCount(socket) })
     socket.on('getLatestBlocks', () => { getLatestBlocks(socket) })
+    socket.on('getBlock', (hashOrIndex) => { getBlock(socket, hashOrIndex) })
+    socket.on('getTransaction', (hash) => { getTransaction(socket, hash) })
+    socket.on('getBlockOrTransaction', (hashOrIndex) => { getBlock(socket, hashOrIndex) })
   })
 }
 
@@ -18,6 +21,51 @@ function getBlockCount(socket) {
 function getLatestBlocks(socket) {
   const latestBlocks = bitcoinClient.cache.latestBlocks
   socket.emit('latestBlocks', latestBlocks)
+}
+
+async function getBlock(socket, hashOrIndex) {
+  let block;
+
+  if (isSha256(hashOrIndex)) {
+    block = await bitcoinClient.getBlock(hashOrIndex)
+  } else if (isNumber(hashOrIndex)) {
+    const blockHash = await bitcoinClient.getBlockHash(hashOrIndex)
+    block = await bitcoinClient.getBlock(blockHash)
+  }
+  socket.emit('block', block)
+}
+
+async function getTransaction(socket, hash) {
+  const transaction = await bitcoinClient.getTransaction(hash)
+  socket.emit('transaction', transaction)
+}
+
+// can't recognize a block hash from a tx hash
+async function getBlockOrTransaction(hashOrIndex) {
+  let block;
+  let transaction;
+
+  if (isSha256(hashOrIndex)) {
+    if (isLikelyBlockHash(hashOrIndex)) block = await bitcoinClient.getBlock(hashOrIndex)
+    //if (!block) transaction = await bitcoinClient.getTransaction(hashOrIndex)
+  } else if (isNumber(hashOrIndex)) {
+    const blockHash = await bitcoinClient.getBlockHash(hashOrIndex)
+    block = await bitcoinClient.getBlock(blockHash)
+  }
+
+  return { block, transaction }
+}
+
+function isSha256(string) {
+  return string.matches("^[a-fA-F0-9]{64}$")
+}
+
+function isNumber(string) {
+  return !isNaN(string)
+}
+
+function isLikelyBlockHash(hash) {
+  return hash.substring(0, 4) === '0000'
 }
 
 bitcoinClient.updateCache()
