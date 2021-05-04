5c043e3ed239d806d49d86131a5882b6879e295e
Mick <mickvd99@gmail.com>
2021-05-04 17:20:50 +0200

    Fixed mempool chart name in mempool view

--- frontend/src/views/MempoolView.vue
+++ frontend/src/views/MempoolView.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="bg-white overflow-hidden rounded-lg px-4 py-5">
-    <h2 class="text-lg font-medium text-gray-900">Mempool size</h2>
-    <MempoolTransactions class="h-80" />
+  <div class="mt-3 grid grid-cols-1 md:grid-cols-1 gap-3">
+    <div class="px-6 py-4 bg-white rounded-lg overflow-hidden">
+      <h2 class="text-lg font-medium text-bitcoin-900 py-1">Mempool Transactions</h2>
+      <MempoolTransactions class="h-80" />
+    </div>
   </div>
 </template>
 
