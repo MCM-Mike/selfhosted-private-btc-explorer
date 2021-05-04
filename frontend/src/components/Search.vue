a373ca2aed078b836d06ae3080e8ec212edc1315
Mick <mickvd99@gmail.com>
2021-05-04 16:53:02 +0200

    Changed search color

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -5,7 +5,7 @@
       <div class="relative">
         <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <!-- Heroicon name: solid/search -->
-          <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+          <svg class="h-5 w-5 text-bitcoin-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
                aria-hidden="true">
             <path fill-rule="evenodd"
                   d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
@@ -14,7 +14,7 @@
         </div>
         <form @submit.prevent="search">
           <input id="search" name="search"
-                 class="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 text-gray-300 placeholder-gray-400 focus:outline-none focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm"
+                 class="block w-full pl-10 pr-3 py-2 border border-transparent rounded-lg leading-5 text-gray-500 placeholder-gray-400 focus:outline-none focus:border-white focus:ring-white focus:text-gray-900 sm:text-sm"
                  placeholder="Hash or Address" type="search" v-model="query"
           >
         </form>
@@ -51,3 +51,6 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+</style>
